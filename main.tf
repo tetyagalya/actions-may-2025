7446d3973623003d49def2cbc3a2db34704ec21b
tetyagalya <salta.dzhusupova@gmail.com>
2025-08-11 19:01:19 +0000

    fix

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource aws_instance web {
     ami           = "ami-08ca1d1e465fbfe0c"
     instance_type = "t3.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-saltanatd"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
